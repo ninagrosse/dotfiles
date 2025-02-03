162aef1974cef616e28eac330449862ad7d20814
Nina Große <nina.grosse@posteo.de>
2025-02-03 18:01:56 +0100

    feat: add packages to install_packages.sh
    Homebrew only (so far)

--- run_onchange_02-install_packages.sh
+++ run_onchange_02-install_packages.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env zsh
 
-brew list
+# Source oh-my-zsh brew plugin, so brew shellenv is initialized for the first time and
+# we can use 'brew bundle' to install packages
+source ${ZSH:-~/.oh-my-zsh}/plugins/brew/brew.plugin.zsh
+
+# Install packages with inline 'brew bundle'
+brew bundle --no-lock --file=/dev/stdin <<EOF
+tap 'mike-engel/jwt-cli'
+tap 'jesseduffield/lazydocker'
+tap 'jesseduffield/lazygit'
+brew 'atuin'
+brew 'bat'
+brew 'btop'
+brew "direnv"
+brew 'dysk'
+brew 'eza'
+brew 'fastfetch'
+brew 'fd'
+brew 'fzf'
+brew 'git-delta'
+brew 'httpie'
+brew 'jq'
+brew 'jesseduffield/lazydocker/lazydocker'
+brew 'jesseduffield/lazygit/lazygit'
+brew 'mike-engel/jwt-cli/jwt-cli'
+brew 'neovim'
+brew 'ripgrep'
+brew 'starship'
+brew "tealdeer"
+brew "tmux"
+brew "ugrep"
+brew 'wget'
+brew "xxh"
+brew 'yazi'
+brew "zellij"
+brew "zoxide"
+EOF
